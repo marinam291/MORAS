bf81dbab08c37cb436bb4d2f419620b501ccf128
marinam291 <marenjakmarina@gmail.com>
2024-11-18 17:26:53 +0100

    Delete dokaz.v

--- dokaz.v
+++
@@ -1,8 +0,0 @@
-Theorem prvia:
-  forall (x y z: bool), 
-    orb (orb (andb x (negb y)) (andb (negb x) (negb y))) (andb (negb x) y) = negb (andb x y).
-Proof.
-  intros x y z.
-  destruct x, y; simpl; reflexivity.
-Qed.
-  
